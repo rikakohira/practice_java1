973242cb1c2d62b3824a3036b2e843ff71394711
rikakohira <rikako.takase88@gmail.com>
2022-10-20 13:15:32 +0900

    整数

--- Greeting.java
+++ Greeting.java
@@ -4,14 +4,24 @@ class Greeting {
     // System.out.println("Good afternoon");
     // System.out.println("good evening");
 
-    int var = 1;
-    int var01 = 2;
-    int var_01 = 3;
-    int var$01 = 4;
-    System.out.println(var);
+    // int VAR = 1;
+    // int Var = 2;
+    // int var_01 = 3;
+    // int var$01 = 4;
+    // System.out.println(VAR);
+    // System.out.println(Var);
+    // System.out.println(var_01);
+    // System.out.println(var$01);
+
+    byte var01 = 1;
+    short var02 = 12345;
+    int var03 = 1234567890;
+    long var04 = 123456789000L;
+
     System.out.println(var01);
-    System.out.println(var_01);
-    System.out.println(var$01);
+    System.out.println(var02);
+    System.out.println(var03);
+    System.out.println(var04);
 
   }
 }
