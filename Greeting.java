4719d2ff171000052c27ba10f608c1f15cbef766
rikakohira <rikako.takase88@gmail.com>
2022-10-20 12:43:42 +0900

    変数

--- Greeting.java
+++ Greeting.java
@@ -1,9 +1,17 @@
 class Greeting {
   public static void main(String[] args) {
-    System.out.println("Good morning");
+    // System.out.println("Good morning");
+    // System.out.println("Good afternoon");
+    // System.out.println("good evening");
 
-    System.out.println("Good afternoon");
+    int var = 1;
+    int var01 = 2;
+    int var_01 = 3;
+    int var$01 = 4;
+    System.out.println(var);
+    System.out.println(var01);
+    System.out.println(var_01);
+    System.out.println(var$01);
 
-    System.out.println("good evening");
   }
 }
