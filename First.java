2980c9f83ea8747fca536ca52f8e148e88f58d3c
rikakohira <rikako.takase88@gmail.com>
2022-10-21 19:39:55 +0900

    変数

--- First.java
+++ First.java
@@ -1,5 +1,12 @@
 class First {
   public static void main(String[] args) {
     System.out.println("Javaを楽しもう");
+
+    int month;
+    month = 9;
+    int day = 21;
+
+    System.out.println("このチャンネルは、");
+    System.out.println(month + "月" + day + "日に公開しました！");
   }
 }
