fc17221dc9a32c0e1f44a8ea0c5049e35072c441
rikakohira <rikako.takase88@gmail.com>
2022-10-21 20:37:41 +0900

    演習問題4

--- First.java
+++ First.java
@@ -1,34 +1,43 @@
 class First {
   public static void main(String[] args) {
-    System.out.println("Javaを楽しもう");
+    // System.out.println("Javaを楽しもう");
 
-    int month;
-    month = 9;
-    int day = 21;
+    // int month;
+    // month = 9;
+    // int day = 21;
 
-    System.out.println("このチャンネルは、");
-    System.out.println(month + "月" + day + "日に公開しました！");
+    // System.out.println("このチャンネルは、");
+    // System.out.println(month + "月" + day + "日に公開しました！");
 
-    int[] score = new int[3];
+    // int[] score = new int[3];
+    // score[0] = 90;
+    // score[1] = 20;
+    // score[2] = 30;
+
+    // String[] name = { "高瀬", "田中", "磯野" };
+
+    // System.out.println(name[0] + "さんは、" + score[0] + "点でした！");
+
+    // int num = 10;
+    // double pi = 3.14;
+    // char blood = 'A';
+    // boolean fun = true;
+    // String msg = "こんにちは！";
+
+    // System.out.println(num);
+    // System.out.println(pi);
+    // System.out.println(blood);
+    // System.out.println(fun);
+    // System.out.println(msg);
+
+    int[] score = new int[4];
+
     score[0] = 90;
-    score[1] = 20;
-    score[2] = 30;
+    score[1] = 80;
+    score[2] = 100;
+    score[3] = 90;
 
-    String[] name = { "高瀬", "田中", "磯野" };
-
-    System.out.println(name[0] + "さんは、" + score[0] + "点でした！");
-
-    int num = 10;
-    double pi = 3.14;
-    char blood = 'A';
-    boolean fun = true;
-    String msg = "こんにちは！";
-
-    System.out.println(num);
-    System.out.println(pi);
-    System.out.println(blood);
-    System.out.println(fun);
-    System.out.println(msg);
+    System.out.println("一人目は" + score[0] + "点です");
 
   }
 }
