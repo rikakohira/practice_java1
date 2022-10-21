2f4373506cc8e9bb7c006e8563abe225e907fcba
rikakohira <rikako.takase88@gmail.com>
2022-10-21 22:31:10 +0900

    演算子

--- First.java
+++ First.java
@@ -30,14 +30,25 @@ class First {
     // System.out.println(fun);
     // System.out.println(msg);
 
-    int[] score = new int[4];
+    // int[] score = new int[4];
 
-    score[0] = 90;
-    score[1] = 80;
-    score[2] = 100;
-    score[3] = 90;
+    // score[0] = 90;
+    // score[1] = 80;
+    // score[2] = 100;
+    // score[3] = 90;
 
-    System.out.println("一人目は" + score[0] + "点です");
+    // System.out.println("一人目は" + score[0] + "点です");
 
+    int a = Integer.parseInt(args[0]);
+    int b = Integer.parseInt(args[1]);
+
+    System.out.println("a = " + a + ", b = " + b);
+    System.out.println("a + b = " + (a + b));
+    System.out.println("a - b = " + (a - b));
+    System.out.println("a * b = " + (a * b));
+    System.out.println("a / b = " + (a / b));
+    System.out.println("a % b = " + (a % b));
+    System.out.println("a >= 0�F" + (a >= 0));
+    System.out.println("(a >= 0) && (b >= 0)�F" + ((a >= 0) && (b >= 0)));
   }
 }
