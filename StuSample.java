e0791572b65a9c58b23c8551d4b9020dc3ff567a
rikakohira <rikako.takase88@gmail.com>
2022-10-24 17:41:31 +0900

    コンストラクタ

--- StuSample.java
+++ StuSample.java
@@ -8,14 +8,21 @@ class StuSample {
     // stu1.display();
     // System.out.println("平均" + stu1.getAvg() + "点");
 
-    Student stu1 = new Student();
-    Student stu2 = new Student();
+    // Student stu1 = new Student();
+    // Student stu2 = new Student();
 
-    stu1.setData("菅原");
+    // stu1.setData("菅原");
+    // stu1.setScore(90, 80);
+    // stu1.display();
+
+    // stu2.setData("村山", 75, 100);
+    // stu2.display();
+
+    Student stu1 = new Student("菅原");
+    Student stu2 = new Student("村山", 75, 100);
+
     stu1.setScore(90, 80);
     stu1.display();
-
-    stu2.setData("村山", 75, 100);
     stu2.display();
   }
 }
